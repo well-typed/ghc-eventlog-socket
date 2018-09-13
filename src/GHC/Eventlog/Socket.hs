6ee546190bb69bca1d3362a77db2ef75a764c18c
Ben Gamari <ben@smart-cactus.org>
2018-09-13 17:55:58 -0400

    Add haddocks

--- src/GHC/Eventlog/Socket.hs
+++ src/GHC/Eventlog/Socket.hs
@@ -4,4 +4,5 @@ module GHC.Eventlog.Socket
     ( start
     ) where
 
-foreign import ccall "eventlog_socket_start" start :: IO ()
+-- | Start listening for eventlog connections, blocking until a client connects.
+foreign import ccall unsafe "eventlog_socket_start" start :: IO ()
